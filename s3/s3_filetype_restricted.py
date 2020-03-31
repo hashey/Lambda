166c404f97e7ff041b3a90707dc5376177b19a82
Justin Kiley <justin.kiley@gmail.com>
2020-03-31 15:39:19 -0500

    Create s3_filetype_restricted.py

---
+++ s3/s3_filetype_restricted.py
@@ -0,0 +1,25 @@
+import json
+import os
+import boto3
+
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+        fileName = event['Records'][0]['s3']['object']['key']
+        fileType = os.path.splitext(fileName)[1]
+        
+        approvedFileTypes = ['.txt','.log']
+        
+        if fileType not in approvedFileTypes:
+                try:
+                        print("Automatically deleting unauthorized content  ", fileName, " - filetype ", fileType, " is not in approved list.")
+
+                        s3bucket = event['Records'][0]['s3']['bucket']['name']
+                        s3key = fileName
+        
+                        s3object = s3.Object(s3bucket, s3key)
+                        s3object.delete()
+                except:
+                        print("Exception encountered attempting to delete ", fileName)
+        else:
+                print("File type ", fileType, " is approved, no action required.")
